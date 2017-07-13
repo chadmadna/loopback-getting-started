7cf614c0797ec5aa7219f507a893f485640a282d
chadmadna <chadmadna@gmail.com>
2017-07-13 21:01:19 +0700

    Restructured client folders

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,13 @@ var source = require('vinyl-source-stream')
 gulp.task('bundle', function () {
   return browserify({
     extensions: ['.js', '.jsx'],
-    entries: './client/js/index.js'
+    entries: './client/app/index.js'
   })
     .transform(babelify.configure({
       ignore: /(bower_components)|(node_modules)/
     }))
     .bundle()
     .on('error', function (err) { console.log('Error : ' + err.message) })
-    .pipe(source('index_bundle.js'))
-    .pipe(gulp.dest(__dirname + './client/js/dist'))
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(__dirname + './client/public'))
 })
